87c89394da3901d31ad85630eb6565c5f4d9597c
koladon52 <59905926+koladon52@users.noreply.github.com>
2020-04-11 21:54:32 +0700

    commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyparser =  require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/myanime', {useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost:27017/myanime', {useNewUrlParser: true});
 app.set("view engine","ejs");
 app.use(bodyparser.urlencoded({extended : true}));
 app.use(express.static("public"));
@@ -13,13 +13,13 @@ app.get("/", function(req, res){
     res.render("landing");
 })
 
-let animeschema = new mongoose.Schema({
-    name: String,
-    imgurl: String,
-    desc: String
-})
+// let animeschema = new mongoose.Schema({
+//     name: String,
+//     imgurl: String,
+//     desc: String
+// })
 
-let Anime = mongoose.model("Anime",animeschema);
+// let Anime = mongoose.model("Anime",animeschema);
 
 // Anime.create(
 //     {
@@ -46,6 +46,22 @@ let Anime = mongoose.model("Anime",animeschema);
 //     {name: "Mahouka Koukou no Rettousei: Raihousha-hen",imgurl: "https://cdn.myanimelist.net/images/anime/1788/106668.jpg",value:"8"}
 // ];
 
+app.get("/Resume", function(req, res){
+    res.render("Resume");
+})
+
+app.get("/Find_a_job", function(req, res){
+    res.render("Find_a_job");
+})
+
+app.get("/History", function(req, res){
+    res.render("History");
+})
+
+app.get("/Liked", function(req, res){
+    res.render("Liked");
+})
+
 app.get("/login", function(req, res){
     res.render("login");
 })
@@ -54,44 +70,44 @@ app.get("/register", function(req, res){
     res.render("register");
 })
 
-app.get("/anime/new", function(req, res){
-    res.render("addnewanime");
-})
+// app.get("/anime/new", function(req, res){
+//     res.render("addnewanime");
+// })
 
-app.get("/anime/:id", function(req,res){
-    Anime.findById(req.params.id, function(error, animedetail){
-        if(error){
-            console.log("ERROR1")
-        }else{
-            res.render("showdetail", {anime:animedetail});
-        }
-    });
-});
+// app.get("/anime/:id", function(req,res){
+//     Anime.findById(req.params.id, function(error, animedetail){
+//         if(error){
+//             console.log("ERROR1")
+//         }else{
+//             res.render("showdetail", {anime:animedetail});
+//         }
+//     });
+// });
 
-app.get("/index", function(req, res){
-    Anime.find({},function(error, allAnime){
-        if(error){
-            console.log("ERROR2");
-        }else{
-            res.render("collection", {anime:allAnime});
-        }
-    })
+// app.get("/index", function(req, res){
+//     Anime.find({},function(error, allAnime){
+//         if(error){
+//             console.log("ERROR2");
+//         }else{
+//             res.render("collection", {anime:allAnime});
+//         }
+//     })
     
-})
+// })
 
-app.post("/index", function(req, res){
-    let n_name = req.body.name;
-    let n_image = req.body.imgurl;
-    let n_anime = {name:n_name,imgurl:n_image}
-    Anime.create(n_anime, function(error, newanime){
-        if(error){
-            console.log("error")
-        }else{
-            console.log("NEW ANIME ADDED");
-            res.redirect("/index");
-        }
-    })
-})
+// app.post("/index", function(req, res){
+//     let n_name = req.body.name;
+//     let n_image = req.body.imgurl;
+//     let n_anime = {name:n_name,imgurl:n_image}
+//     Anime.create(n_anime, function(error, newanime){
+//         if(error){
+//             console.log("error")
+//         }else{
+//             console.log("NEW ANIME ADDED");
+//             res.redirect("/index");
+//         }
+//     })
+// })
 
 app.listen(3000, function(res,req){
     console.log("MY COLLECTION")

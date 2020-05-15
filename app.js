1129befe03da160a15b9cf68ff9a7a5246825a62
koladon52 <59905926+koladon52@users.noreply.github.com>
2020-05-15 21:47:56 +0700

    commit

--- app.js
+++ app.js
@@ -1,118 +1,100 @@
-const express = require("express");
-const path = require("path");
-const bodyparser =  require("body-parser");
-const app = express();
-const mongoose = require("mongoose");
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({extended:false});
+const express = require("express"),
+      path = require("path"),
+      bodyParser =  require("body-parser"),
+      mongoose = require("mongoose"),
+      passport = require('passport'),
+      passportlocal = require('passport-local')
+      passportlocalMongoose = require('passport-local-mongoose')
+      User = require('./models/user') 
+      ;
 
-mongoose.connect('mongodb://localhost:27017/myanime', {useNewUrlParser: true});
-app.set("view engine","ejs");
-app.use(bodyparser.urlencoded({extended : true}));
+let app = express();
+
+mongoose.connect('mongodb://localhost:27017/projectweb');
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(require('express-session')({
+    secret: 'CSS227',
+    resave: false,
+    saveIninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+passport.use(new passportlocal(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
     res.render("landing");
 })
 
-// let animeschema = new mongoose.Schema({
-//     name: String,
-//     imgurl: String,
-//     desc: String
-// })
-
-// let Anime = mongoose.model("Anime",animeschema);
-
-// Anime.create(
-//     {
-//     name: "Yahari",
-//     imgurl: "https://cdn.myanimelist.net/images/anime/1910/106073.jpg",
-//     desc:"sodeeeee"
-//     }, function(error, anime){
-//         if(error){
-//             console.log("ERROR!");
-//         }else{
-//             console.log("ADDED");
-//             console.log(anime);
-//         }   
-// });
-
-// let = anime = [ 
-//     {name: "Re-Zero",imgurl: "https://cdn.myanimelist.net/images/anime/1010/100084.jpg",value:"1"},
-//     {name: "Yahari ",imgurl: "https://cdn.myanimelist.net/images/anime/1910/106073.jpg",value:"2"},
-//     {name: "SAO",imgurl: "https://cdn.myanimelist.net/images/anime/1438/105106.jpg",value:"3"},
-//     {name: "Maou Gakuin no Futekigousha",imgurl: "https://cdn.myanimelist.net/images/anime/1388/105332.jpg",value:"4"},
-//     {name: "Princess Connect Re:Dive",imgurl: "https://cdn.myanimelist.net/images/anime/1810/106070.jpg",value:"5"},
-//     {name: "Kanojo, Okarishimasu",imgurl: "https://cdn.myanimelist.net/images/anime/1920/104817.jpg",value:"6"},
-//     {name: "Tonikaku Kawaii",imgurl: "https://cdn.myanimelist.net/images/anime/1279/106298.jpg",value:"7"},
-//     {name: "Mahouka Koukou no Rettousei: Raihousha-hen",imgurl: "https://cdn.myanimelist.net/images/anime/1788/106668.jpg",value:"8"}
-// ];
-
-app.get("/Resume", function(req, res){
+app.get("/Resume",isloggedIn, function(req, res){
     res.render("Resume");
 })
 
-app.get("/Find_a_job", function(req, res){
+app.get("/Find_a_job", isloggedIn, function(req, res){
     res.render("Find_a_job");
 })
 
-app.get("/History", function(req, res){
+app.get("/History",isloggedIn, function(req, res){
     res.render("History");
 })
 
-app.get("/Liked", function(req, res){
+app.get("/Liked",isloggedIn, function(req, res){
     res.render("Liked");
 })
 
-app.get("/login", function(req, res){
-    res.render("login");
-})
+// ---------Authen--------------
 
-app.get("/register", function(req, res){
-    res.render("register");
-})
+app.get('/login', function(req, res){
+    res.render('Autherization/login');
+});
 
-// app.get("/anime/new", function(req, res){
-//     res.render("addnewanime");
-// })
+app.post('/login', passport.authenticate('local',{
+    successRedirect: '/',
+    failureRedirect: 'Autherization/login'
+}),function(req, res){
 
-// app.get("/anime/:id", function(req,res){
-//     Anime.findById(req.params.id, function(error, animedetail){
-//         if(error){
-//             console.log("ERROR1")
-//         }else{
-//             res.render("showdetail", {anime:animedetail});
-//         }
-//     });
-// });
-
-// app.get("/index", function(req, res){
-//     Anime.find({},function(error, allAnime){
-//         if(error){
-//             console.log("ERROR2");
-//         }else{
-//             res.render("collection", {anime:allAnime});
-//         }
-//     })
-    
-// })
-
-// app.post("/index", function(req, res){
-//     let n_name = req.body.name;
-//     let n_image = req.body.imgurl;
-//     let n_anime = {name:n_name,imgurl:n_image}
-//     Anime.create(n_anime, function(error, newanime){
-//         if(error){
-//             console.log("error")
-//         }else{
-//             console.log("NEW ANIME ADDED");
-//             res.redirect("/index");
-//         }
-//     })
-// })
+});
 
 
 
+function isloggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
+
+app.get('/signup', function(req, res){
+    res.render('Autherization/signup');
+});
+
+app.post('/signup', function(req, res){
+    User.register(new User({username: req.body.username, email:req.body.email, type:req.body.Type}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('Autherization/signup');
+        }
+        passport.authenticate('local')(req,res,function(){
+            res.redirect('/');
+        });
+    });
+});
+
+app.get('/logout',isloggedIn ,function(req, res){
+    req.logout();
+    res.redirect('/');
+});
 
 app.listen(3000, function(res,req){
     console.log("MY COLLECTION")
---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose'),
+      passportlocalMongoose = require('passport-local-mongoose')
+      ;
+
+let UserSchema = new mongoose.Schema({
+    username : String,
+    email    : String,
+    password : String,
+    type     : String
+})
+
+UserSchema.plugin(passportlocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById=function(id,callback){
+    User.findById(id,callback);
+}
+
+module.exports.getUserByName=function(id,callback){
+    var query = {
+        username : username,
+        type : Type
+    };
+    User.findOne(query,callback);
+}

912e9ea2ac83f3cd13ba27d9d323afde11203ecc
koladon52 <59905926+koladon52@users.noreply.github.com>
2020-06-25 07:22:45 +0700

    6

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,6 @@ var express  =  require('express'),
     mongoose = require("mongoose"),
     
     
-mongoose.connect('mongodb://localhost:27017/projectweb',{ useNewUrlParser: true })
 
 router.get('/projects' , function(req, res , next){
     res.render("/");  

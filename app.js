a68cf23ac34a4e33614e88a5ca507f9b76ebb67d
koladon52 <59905926+koladon52@users.noreply.github.com>
2020-06-25 08:18:29 +0700

    14

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(function(req, res, next) {
   });
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Views')));
 
 mongoose.set('useUnifiedTopology',true);
 mongoose.set('useCreateIndex',true);
